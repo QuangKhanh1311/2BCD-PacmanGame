70a146f8aa8153532d43e3b97d72c1e5c6447a7d
QuangKhanh1311 <148845473+QuangKhanh1311@users.noreply.github.com>
2024-10-28 21:01:38 +0700

    full

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameRunForm));
             this.txtScore = new System.Windows.Forms.Label();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.ice1 = new System.Windows.Forms.PictureBox();
@@ -702,6 +703,7 @@
             this.Controls.Add(this.bigCoins3);
             this.Controls.Add(this.bigCoins2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "GameRunForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
--- GameStartForm.Designer.cs
+++ GameStartForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameStartForm));
             this.btnHard = new System.Windows.Forms.Button();
             this.btnEasy = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
@@ -161,6 +162,7 @@
             this.Controls.Add(this.btnHard);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "GameStartForm";
             this.Text = "Pacman 2BCD";
